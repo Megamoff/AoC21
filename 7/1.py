8e131613410438a40f47c148dc82370f939cb11c
Megamoff <patrick.manegold@uni-duesseldorf.de>
2021-12-07 14:35:39 +0100

    day 7

---
+++ 7/1.py
@@ -0,0 +1,16 @@
+data = open("input.txt").read().split(",")
+
+pos = [0]*2000
+for i in data:
+	pos[int(i)] += 1
+
+low = 100000000
+for j in range(len(pos)):
+	res = 0
+	for i in range(len(pos)):
+		res += pos[i] * abs(i - j)
+	if res < low:
+		low = res
+	else:
+		break
+print(low)
